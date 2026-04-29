9e1dde3fa569828b46b8faa7588c00d3f81c02a5
IGdotnet <belonfranzolinigor@gmail.com>
2026-04-29 20:13:12 -0300

    Selecting a schedule to delete

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,24 @@
+const periods = document.querySelectorAll(".period")
+
+//Generate click event for each list
+periods.forEach((periods) => {
+    //Capture the click event from the list
+    periods.addEventListener("click", (event) => {
+        //console.log(event.target.classList.contains("cancel-icon"))
+        if(event.target.classList.contains("cancel-icon")){
+            //Get the li from the clicked element
+            const item = event.target.closest("li")
+            //onsole.log(item)
+            const { id } = item.dataset
+            //console.log(id)
+
+            if(id){
+                const isConfirm = confirm("Tem certeza que deseja cancelar esse agendamento?")
+                            
+                //if(isConfirm){
+                //    console.log("Confirmado")
+                //}
+            }
+        }
+    })
+})
