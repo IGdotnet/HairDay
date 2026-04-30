24d292d6226aad4d5b441188c85c974c761b89ad
IGdotnet <belonfranzolinigor@gmail.com>
2026-04-29 21:24:50 -0300

    Removing a schedule from API

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,9 +1,12 @@
+import { schedulesDay } from "./load.js"
+import { scheduleCancel } from "../../services/schedule-cancel"
+
 const periods = document.querySelectorAll(".period")
 
 //Generate click event for each list
 periods.forEach((periods) => {
     //Capture the click event from the list
-    periods.addEventListener("click", (event) => {
+    periods.addEventListener("click", async (event) => {
         //console.log(event.target.classList.contains("cancel-icon"))
         if(event.target.classList.contains("cancel-icon")){
             //Get the li from the clicked element
@@ -18,6 +21,14 @@ periods.forEach((periods) => {
                 //if(isConfirm){
                 //    console.log("Confirmado")
                 //}
+
+                //Make the request to API to cancel
+                if(isConfirm){
+                    await scheduleCancel ({ id })
+
+                    //Reload the list
+                    schedulesDay()
+                }
             }
         }
     })
